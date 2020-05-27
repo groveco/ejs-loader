df10932c53894713381c323a0fca167486ab3ee9
AtofStryker <bglesias@grove.co>
2020-05-27 17:47:19 -0400

    inform user that 'variable' option is required for es6 transformations

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import _ from 'lodash';
 import loaderUtils from'loader-utils';
 
+class UnsupportedConfiguration extends Error {
+  constructor(message){
+    super(message)
+  }
+}
+
 function getOptions(context) {
   if (context.options && context.options.ejsLoader) {
     return context.options.ejsLoader;
@@ -12,6 +18,14 @@ export default function (source){
   this.cacheable && this.cacheable();
   var query = loaderUtils.parseQuery(this.query);
   var options = getOptions(this);
+
+  if(!options.variable && !query.variable){
+    throw new UnsupportedConfiguration(`
+      To support ES Modules, the 'variable' option must be passed to avoid 'with' statements 
+      in the compiled template to be strict mode compatible. 
+      Please see https://github.com/lodash/lodash/issues/3709#issuecomment-375898111
+    `)
+  }
 
   ['escape', 'interpolate', 'evaluate'].forEach(function(templateSetting) {
     var setting = query[templateSetting];
